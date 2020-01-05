01af18ee10dd97182987cc9eb03b9ca88cbe7504
Scales <Scalesrzn@gmail.com>
2020-01-05 16:05:28 +0300

    last changes

--- top.inc.php
+++ top.inc.php
@@ -1,3 +1,7 @@
 
     <div class='label'> <img src="../gallery/label.png" alt="ШПАК"> </div>
     <div> <span>Частная зубная поликлиника "Шпак"</span> </div>
+    <?php
+	    if (!empty($_SESSION['user_login']))
+		    echo "<div class='welcomemsg'>Добро пожаловать, <b>{$_SESSION['user_login']}!</b> <a class='button' href='index.php?logout=true'>  (Выйти)</a></div>";
+    ?>
